4536488b91564ebe966a5bc19d9be5e8d8bdb5bb
wanglei <806416060@qq.com>
2021-11-23 16:43:32 +0800

    新建项目，引入样式组件，

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import {IconStyle} from './assets/iconfont/iconfont'
+import { GlocalStyle } from './style';
+
+function App() {
+  return (
+    <div className="App">
+      <GlocalStyle />
+      <IconStyle />
+      <i className="iconfont">&#xe62b;</i>
+    </div>
+  );
+}
+
+export default App;
