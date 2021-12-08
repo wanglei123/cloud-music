f5a2950d25374aa1062f4d784a018f2ffbfebcec
wanglei <806416060@qq.com>
2021-12-08 17:27:43 +0800

    增加better-scroll组件

--- src/application/Recommend/Recommend.js
+++ src/application/Recommend/Recommend.js
@@ -1,34 +1,53 @@
 /*
  * @Author: your name
  * @Date: 2021-11-24 10:15:52
- * @LastEditTime: 2021-12-06 15:18:58
+ * @LastEditTime: 2021-12-08 17:22:29
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /cloud-music/src/application/Recommend/Recommend.js
  */
-import React from 'react'
+import React from 'react';
 
-import Slider from '../../components/slider'
-import RecommendList from '../../components/list'
+import Slider from '../../components/slider';
+import RecommendList from '../../components/list';
+import Scroll from '../../components/scroll';
+import styled from 'styled-components';
+
+const Content = styled.div `
+  position: fixed;
+  top: 90px;
+  bottom: 0px;
+  width: 100%;
+`
 
 function Recommend() {
   // mock数据
-  const bannerList = [1,2,3,4].map(item => {
-    return { imageUrl: "http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg" }
-  })
+  const bannerList = [1, 2, 3, 4].map((item) => {
+    return {
+      imageUrl:
+        'http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg',
+    };
+  });
 
-  const recommendList = [1,2,3,4,5,6,7,8,9,10].map(item => {
+  const recommendList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => {
     return {
       id: 1,
-      picUrl: 'https://p1.music.126.net/fhmefjUfMD-8qtj3JKeHbA==/18999560928537533.jpg',
+      picUrl:
+        'https://p1.music.126.net/fhmefjUfMD-8qtj3JKeHbA==/18999560928537533.jpg',
       playCount: 17171122,
-      name: '朴树、许巍、李健、郑钧、老狼、赵雷'
-    }
-  })
-  return ( <div>
-    <Slider bannerList={bannerList}></Slider>
-    <RecommendList recommendList={recommendList}></RecommendList>
-  </div> );
+      name: '朴树、许巍、李健、郑钧、老狼、赵雷',
+    };
+  });
+  return (
+    <Content>
+      <Scroll>
+        <div>
+          <Slider bannerList={bannerList}></Slider>
+          <RecommendList recommendList={recommendList}></RecommendList>
+        </div>
+      </Scroll>
+    </Content>
+  );
 }
 
 export default React.memo(Recommend);
